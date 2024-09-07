25c5fa449331274d674a00ccc0233f7fb4590611
Eugene Ginaylo <ginaylo.eugene@gmail.com>
2024-09-07 15:53:25 +0300

    Add github actions

---
+++ infra/setup/ecr.tf
@@ -0,0 +1,24 @@
+######################################
+# Create ECR repos for storin images #
+
+resource "aws_ecr_repository" "app" {
+  name                 = "recipe-app-api-app"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    # enable on productoin
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_repository" "proxy" {
+  name                 = "recipe-app-api-proxy"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    # enable on productoin
+    scan_on_push = false
+  }
+}
